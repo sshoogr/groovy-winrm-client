c84f08d7239592dfa72a1b7738cd034110e4ec9d
Andrey Adamovich <andrey@aestasit.com>
2014-11-26 09:49:33 +0200

    adjusted build

--- src/test/groovy/com/aestasit/infrastructure/winrm/CoolTest.groovy
+++ src/test/groovy/com/aestasit/infrastructure/winrm/CoolTest.groovy
@@ -13,3 +13,16 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+package com.aestasit.infrastructure.winrm
+
+import org.junit.Test
+
+class CoolTest {
+
+  @Test
+  void testCoolStuff() {
+
+  }
+
+}
